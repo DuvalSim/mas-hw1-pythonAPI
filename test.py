5a1ccf581d3ce16a6285fadddad420a637cad7a1
DuvalSim <39551986+DuvalSim@users.noreply.github.com>
2021-02-06 14:59:28 -0500

    Update test.py
    Remove sensitive content

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 import pyrebase
 
 config = {
-  "apiKey": "AIzaSyAQXySEkoPmkUhXsebjLbRv7Ri9NeS4RWo",
-  "authDomain": "mas-hw1.firebaseapp.com",
-  "databaseURL": "https://mas-hw1-default-rtdb.firebaseio.com",
-  "storageBucket": "mas-hw1.appspot.com"
+  "apiKey": "",
+  "authDomain": "",
+  "databaseURL": "",
+  "storageBucket": ""
 }
 
 firebase = pyrebase.initialize_app(config)
 
 db = firebase.database()
 print(db.child("users").get().val())
-data = {"username": "Tintin ", "id":"2"}
-db.child("users").child("Simon").set(data)
